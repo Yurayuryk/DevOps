d64498ac0d474ab5831e7d62a10d0434b1da5732
YuriiRakhimov <yyuuraganov@gmail.com>
2019-12-17 13:17:05 +0200

    Added wordpress-docker

---
+++ VagProject1/docker.sh
@@ -0,0 +1,41 @@
+
+sudo yum check-update
+sudo curl -fsSL https://get.docker.com/ | sh
+sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)"  -o /usr/local/bin/docker-compose
+sudo mv /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
+sudo groupadd docker
+sudo usermod -aG docker ${USER}
+sudo systemctl start docker
+sudo systemctl status docker
+mkdir wordpress
+cd wordpress
+echo "version: '2'
+
+services:
+   db:
+     image: mysql:5.7
+     volumes:
+       - db_data:/var/lib/mysql
+     restart: always
+     environment:
+       MYSQL_ROOT_PASSWORD: somewordpress
+       MYSQL_DATABASE: wordpress
+       MYSQL_USER: wordpress
+       MYSQL_PASSWORD: wordpress
+
+   wordpress:
+     depends_on:
+       - db
+     image: wordpress:latest
+     ports:
+       - "8000:80"
+     restart: always
+     environment:
+       WORDPRESS_DB_HOST: db:3306
+       WORDPRESS_DB_USER: wordpress
+       WORDPRESS_DB_PASSWORD: wordpress
+volumes:
+    db_data:" >> docker-compose.yml
+docker-compose up -d
+
